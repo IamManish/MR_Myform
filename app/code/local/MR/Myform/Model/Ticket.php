013eb50f2195770c2a65651e8782ed62091fb009
manish24online <manish.ranjan@overcart.com>
2017-10-31 00:51:30 +0530

    Added files

---
+++ app/code/local/MR/Myform/Model/Ticket.php
@@ -0,0 +1,10 @@
+<?php
+ 
+class MR_Myform_Model_Ticket extends Mage_Core_Model_Abstract
+{
+    public function _construct()
+    {
+        parent::_construct();
+        $this->_init('myform/ticket');
+    }
+}
